548dbeda46f10f842b4e0de0903de521e2015e2d
Maxim Medvedev <m.a.medvedev@outlook.com>
2022-07-04 14:27:03 +0500

    Variable_names.py

---
+++ Variable_names.py
@@ -0,0 +1,35 @@
+before - node_above_current
+// определение узла связанного списка, ссылающегося на текущий
+
+node - current_node
+// текущий узел связанного списка
+
+kys - keys_of_dictionary
+// ключи словаря, отбираемые по индексу
+
+repeat - dictionary_to_find_repeatings
+// словарь со значениями, которые необходимо подсчитать по N
+
+name - name_of_parent_class
+// переменная имени основного класса
+
+i - line_position
+// позиция по горизонтали при переборе массива
+
+j - column_psition
+// позиция по вертикали при переборе массива
+
+c - current_index_from_string
+// текущее значение индекса, содержащееся в строке
+
+self._salary - self._current_salary_amount
+// текущее значение зарплаты
+
+self.__candidate - self.__is_person_in_trial_period
+// булева переменная, обозначающая находится ли сотрудник на испытательном сроке
+
+dictionary - dictionary_of_symbols_codes
+// словарь замены кодов символов
+
+discount - total_discount
+// итоговое значение скидки
