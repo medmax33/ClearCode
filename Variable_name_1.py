b56ba9bcd828a7ad4f2bddab6b18dc5aa5afe36c
Maxim Medvedev <m.a.medvedev@outlook.com>
2022-07-06 15:00:32 +0500

    task 2 - 6.1-6.4 - Variable_name_1.py

---
+++ Variable_name_1.py
@@ -0,0 +1,50 @@
+6.1
+summa - summ_of_all_ord_in_string
+// сумма всех численных значений символов в строке
+
+index - result_of_hash_function
+// непустой индекс массива в результате расчета хэш функции
+
+array1 - game_field
+// массив значений, описывающих все игровое поле
+
+array2 - inner_array
+// массив значений, по которым определяется вхождение в игровое поле
+
+disc_block - discounts_left
+// количество возможных скидок
+
+6.2
+hash_fun
+// метод определения хэш функции
+
+Linked_list_1 = LinkedList()
+// определение экземпляра класса однонаправленного связанного списка
+
+double_linked_list
+// определдение экземпляра класса двунаправленного связанного списка
+
+stack_array
+// массив со стэком
+
+6.3
+person_name
+// имя физического лица
+
+employee_salary
+// зарплата работника
+
+double_linked_list_head
+// голова двунанаправленного списка
+
+6.4
+name - person_name
+
+head - double_linked_list_head
+
+tail - double_linked_list_tail
+
+node - current_node
+
+candidate - is_person_in_trial_period: bool
+
