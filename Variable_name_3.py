28ea1dc138ed2b3a7cef8aceec8eb031ff155a2e
Maxim Medvedev <m.a.medvedev@outlook.com>
2022-07-11 17:45:09 +0500

    task - 8 - Variable_name_3.py

---
+++ Variable_name_3.py
@@ -0,0 +1,23 @@
+count - current_index
+
+speed - speed_kmh
+
+dictionary - replacement_variants
+
+list - persons_register
+
+result - is_found
+
+name - persons_fio
+
+array - letters_dictionary
+
+next - node_next
+
+index - current_symbol
+
+average - string_average_ord
+
+max - value_max
+
+array_1, array_2 - array_one, array_two
