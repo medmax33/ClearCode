4473fc1a0b09579c19584c1e21e86516c4bd9005
Maxim Medvedev <m.a.medvedev@outlook.com>
2022-07-12 13:54:41 +0500

    ClearCode :: task - 06

---
+++ ClearCode_06.py
@@ -0,0 +1,28 @@
+3.1
+Employee - PersonBaseInformation
+
+Trainee - InternCharacteristic
+
+Staff - WorkerCV
+
+LinkedList - DoubleLinkedList
+
+Dummy - DummyNode
+
+
+3.2
+get - getLastElement
+
+recolor - changeColor
+
+hello - sayHello
+
+transfer - changeDepartment
+
+change_electro - inputElectricityСonsumption_mounth
+
+change_electro - inputElectricityСonsumption_year
+
+close - closeSchool
+
+set_worker - changeWorkerAmount_year
