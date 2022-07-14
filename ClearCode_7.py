98d9e0d9a590fec4e05be6e347521150e9d4a7c2
Maxim Medvedev <m.a.medvedev@outlook.com>
2022-07-14 23:08:11 +0500

    ClearCode :: task - 07

---
+++ ClearCode_7.py
@@ -0,0 +1,35 @@
+salary_rise - cahnge_salary_to
+// метод изменяет значение зарплаты на введенное
+
+hire - set_tarynee_in_staff
+// метод переводящий сотрудника с испытательного срока в штат
+
+end_trial_period - finish_trial_period
+// окончание испытательного срока
+
+transfer - replace_to_department
+// перевод сотрудника в другой отдел
+
+dissmiss - dissmiss_and_zero_salary
+// сотрудник исключается из штата и обнуляется зарплата
+
+hello - get_name
+// получение имени объекта класса
+
+eat - add_food_to
+// увеличение переменной food на значение
+
+recolor - change_color_to
+// изменение переенной цвет на значение
+
+delete - delete_value_once_or_all
+// удаление узла по значению с возможностью выбора только первого значения или всех
+
+insert - insert_afterNode_newNode
+// добавление нового узла после искомого
+
+find - find_and_return_node_by_value
+// поиск и возвращение узла по значению
+
+find_all - find_allValues_and_return_nodeArray
+// поиск всех узлов по значению и возвращение массива этих узлов
