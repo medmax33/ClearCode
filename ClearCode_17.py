012e0fbc230a4379eebb83e184f212f84f64fca4
Maxim Medvedev <m.a.medvedev@outlook.com>
2022-07-27 12:06:52 +0500

    ClearCode :: task - 17

---
+++ ClearCode_17.py
@@ -0,0 +1,89 @@
+1.
+// было
+# Check pair in oxana
+// стало
+# Проверяем четность массива oxana
+// неочевидный комментарий
+
+2.
+neighbor = [0, 1, 0, -1, 0, 1, 0]  # service array for capture cells
+// ранее записал более подробный комментарий
+// 6. Комментарии за закрывающей фигурной скобкой
+
+3.
+# main cycle
+while flag:
+// лишний комментарий
+// 4. Шум
+
+4.
+for nei in range(4):  # cycle to find cells to capture
+// лишний комментарий
+// 6. Комментарии за закрывающей фигурной скобкой
+
+5.
+elif state_square[xi][yi] == 0:  # capture zero cell
+// 7. Избыточные комментарии
+
+6.
+# result salary array filled by zeros
+salary_ordered = salary.copy()
+salary_ordered.sort()
+// 4. Шум
+
+7.
+# create array for formatted lines
+lines = []
+# create array of '0' and '1' for lines with 'subs'
+res = []
+// 7. Избыточные комментарии
+
+8.
+# add new formatted line
+lines.append(s[start:finish])
+// 7. Избыточные комментарии
+
+9.
+summ = int(summ / 2)  # summa of all data is 2 * answer
+// 6. Комментарии за закрывающей фигурной скобкой
+
+10.
+# rebuild string in array
+array = []
+start = 0
+for _ in range(line):
+    finish = start + column
+    array.append(s[start:finish])
+    start = finish
+// эту часть кода оформил в ввиде функции
+// 12. Не используйте комментарии там, где можно использовать функцию или переменную
+
+11.
+# def test_in_example(self):
+#     self.assertEqual(TheRabbitsFoot(12, True), [0, 0, 0, 1, 0, 0, 0])
+#
+# def test_12345(self):
+#     self.assertEqual(TheRabbitsFoot(10, True), [0])
+// 11. Закомментированный код
+
+12.
+# we have a winner?
+if percent[0] == percent[1]:
+    return 'no winner'
+// 1. Неочевидные комментарии
+
+13.
+# majority or minority?
+if percent[0] > 50:
+    return 'majority winner ' + k
+else:
+    return 'minority winner ' + k
+// 1. Неочевидные комментарии
+
+14.
+# print(UFO(2, [1234, 1777], False))
+// 11. Закомментированный код
+
+15.
+distance = 0  # time to drive total_l
+// 7. Избыточные комментарии
